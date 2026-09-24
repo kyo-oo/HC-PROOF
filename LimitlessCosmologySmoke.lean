466395310f2e233295eaffefbb87cf41547b6929
kyo-oo <lordoftreasures7@gmail.com>
2026-09-24 06:01:24 +0530

    test: add RED limitless cosmology smoke test

---
+++ LimitlessCosmologySmoke.lean
@@ -0,0 +1,13 @@
+import GSTLimitlessCosmology
+
+open GSTLimitlessCosmology
+
+#check CosmicCell
+#check CosmicCoef
+#check GlobalCoef
+#check digitShift
+#check carryShift
+#check lefschetz
+#check windowRestrict
+#check windowExtend
+#check compactToGlobal
